dac9e820098d55f2235b2584b376891d9a1d5d6d
Ell-716 <elenabai.2021@gmail.com>
2024-12-10 19:29:43 +0100

    Implement database schema.

--- datamanager/data_manager.py
+++ datamanager/data_manager.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from data_models import User, Movie
 
 
 class DataManagerInterface(ABC):
@@ -8,93 +9,90 @@ class DataManagerInterface(ABC):
     """
 
     @abstractmethod
-    def get_all_users(self):
+    def get_all_users(self) -> list[User]:
         """
         Retrieve a list of all users in the database.
+
         Returns:
-            list: A list of all users with their details.
+            list[User]: A list of all users.
         """
         pass
 
     @abstractmethod
-    def get_user_movies(self, user_id):
+    def get_user_movies(self, user_id: int) -> list[Movie]:
         """
         Retrieve all movies associated with a specific user.
+
         Args:
             user_id (int): The unique identifier of the user.
+
         Returns:
-            list: A list of movies associated with the given user.
+            list[Movie]: A list of movies associated with the user.
         """
         pass
 
     @abstractmethod
-    def add_user(self, user):
+    def add_user(self, user: User) -> None:
         """
         Add a new user to the database.
+
         Args:
-            user (dict): A dictionary containing user details such as name and ID.
-        Returns:
-            None
+            user (User): The user object to add.
         """
         pass
 
     @abstractmethod
-    def delete_user(self, user_id):
+    def delete_user(self, user_id: int) -> None:
         """
         Remove a user from the database.
+
         Args:
-            user_id (int): The unique identifier of the user to be deleted.
-        Returns:
-            None
+            user_id (int): The unique identifier of the user to delete.
         """
         pass
 
     @abstractmethod
-    def update_user(self, user_id):
+    def update_user(self, user_id: int, name: str) -> None:
         """
         Update the details of an existing user.
+
         Args:
-            user_id (int): The unique identifier of the user to be updated.
-        Returns:
-            None
+            user_id (int): The unique identifier of the user to update.
+            name (str): The new name of the user.
         """
         pass
 
     @abstractmethod
-    def add_movie(self, title, director, release_year, rating):
+    def add_movie(self, movie: Movie) -> None:
         """
         Add a new movie to the database.
+
         Args:
-            title (str): The title of the movie.
-            director (str): The director of the movie.
-            release_year (int): The year the movie was released.
-            rating (float): The rating of the movie.
-        Returns:
-            None
+            movie (Movie): The movie object to add.
         """
         pass
 
     @abstractmethod
-    def delete_movie(self, movie_id):
+    def delete_movie(self, movie_id: int) -> None:
         """
         Remove a movie from the database.
+
         Args:
-            movie_id (int): The unique identifier of the movie to be deleted.
-        Returns:
-            None
+            movie_id (int): The unique identifier of the movie to delete.
         """
         pass
 
     @abstractmethod
-    def update_movie(self, title, director, release_year, rating):
+    def update_movie(self, movie_id: int, title: str = None, director: str = None,
+                     release_year: int = None, rating: float = None) -> None:
         """
         Update the details of an existing movie.
+
         Args:
-            title (str): The updated title of the movie.
-            director (str): The updated director of the movie.
-            release_year (int): The updated release year of the movie.
-            rating (float): The updated rating of the movie.
-        Returns:
-            None
+            movie_id (int): The unique identifier of the movie to update.
+            title (str, optional): The new title of the movie.
+            director (str, optional): The new director of the movie.
+            release_year (int, optional): The new release year of the movie.
+            rating (float, optional): The new rating of the movie.
         """
         pass
---
+++ datamanager/data_models.py
@@ -0,0 +1,64 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+# Many-to-many join table
+user_movies = db.Table(
+    'user_movies',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+    db.Column('movie_id', db.Integer, db.ForeignKey('movies.id'), primary_key=True)
+)
+
+class User(db.Model):
+    """
+    Represents a user in the application.
+
+    Attributes:
+        id (int): The unique identifier for the user.
+        name (str): The name of the user.
+        movies (list[Movie]): The list of movies associated with the user.
+    """
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+
+    movies = db.relationship('Movie', secondary=user_movies, back_populates='users')
+
+    def __repr__(self):
+        return f"User(id = {self.id}, name = {self.name})"
+
+    def __str__(self):
+        return f"{self.id}. {self.name}"
+
+
+class Movie(db.Model):
+    """
+    Represents a movie in the application.
+
+    Attributes:
+        id (int): The unique identifier for the movie.
+        title (str): The title of the movie.
+        release_year (int): The year the movie was released (optional).
+        poster (str): A URL or path to the movie's poster image (optional).
+        director (str): The director of the movie (optional).
+        rating (float): The rating of the movie (optional).
+        users (list[User]): The list of users associated with the movie.
+    """
+    __tablename__ = 'movies'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String, nullable=False)
+    release_year = db.Column(db.Integer, nullable=True)
+    poster = db.Column(db.String, nullable=True)
+    director = db.Column(db.String, nullable=True)
+    rating = db.Column(db.Float, nullable=True)
+
+    users = db.relationship('User', secondary=user_movies, back_populates='movies')
+
+    def __repr__(self):
+        return (f"Movie(id = {self.id}, title = {self.title}, release_year = {self.release_year}, "
+                f"poster = {self.poster}, director = {self.director}, rating = {self.rating})")
+
+    def __str__(self):
+        return f"{self.id}. {self.title} ({self.release_year})"
